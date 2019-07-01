6a4448b053af213f26cd5d3f6317d7e18bbbdb85
Mark Sen Dong <46866387+MarkSenDong@users.noreply.github.com>
2019-07-01 13:49:49 +0200

    changed according to coding guide

--- matlab_code_documentation_example.m
+++ matlab_code_documentation_example.m
@@ -13,58 +13,54 @@
 % proper explainations.
 
 % Set the original directory of the images
-original_dir='/volume/data/MUC/MRI/06-June-2019_nii/';
+originalDir='/volume/data/MUC/MRI/06-June-2019_nii/';
 % Set the destination directoy of the images
-copy_dir='/volume/data/MUC/MRI/06-June-2019_pronia/'; 
-% Set the initial bogan_id for the data set. 
-% Beware: every image must have a globally unique bogan_id!
-bogan_id = 2000000;
-
-%% Addpaths 
+copyDir='/volume/data/MUC/MRI/06-June-2019_pronia/'; 
+% Set the initial boganId for the data set. Beware: every image must have a globally unique boganId!
+boganId = 2000000;
+ 
 addpath /opt/PRONIASoftware/Developpment/Main/Utilities
 
-%% Process
 % Create a file for logging any errors occured during the reorganisation process.
-fileID = fopen('copy_errors.txt','w');     
+FILE_ID = fopen('copy_errors.txt','w');     
 
-% Get a list of all folders within the original_dir
-files = dir(original_dir);
 
-% Iterate through every image in the original_dir
+files = dir(originalDir);
+% Iterate through every image in the originalDir
 for k = 3:length(files)
     subjectID = files(k).name;
 
     try
         % Get the name of the image
-        subname = strsplit(subjectID,'.');
-        subname = subname(1,:);
-        subname = char(subname(1));
-        disp(subname);
+        subName = strsplit(subjectID,'.');
+        subName = subName(1,:);
+        subName = char(subName(1));
+        disp(subName);
 
         % Make the child directories
-        system(['mkdir ' copy_dir subname]);
-        system(['mkdir ' copy_dir subname '/Data']);
-        system(['mkdir ' copy_dir subname '/Data/' num2str(bogan_id)]);
-        system(['mkdir ' copy_dir subname '/xml_folder']);
+        system(['mkdir ' copyDir subName]);
+        system(['mkdir ' copyDir subName '/Data']);
+        system(['mkdir ' copyDir subName '/Data/' num2str(boganId)]);
+        system(['mkdir ' copyDir subName '/xml_folder']);
 
         % Write the .xml file
-        ss.(subname).T0.MRI_sMRI = num2str(bogan_id);        
-        xmlpath=[copy_dir subname '/xml_folder'];
-        struct2xml(ss,[xmlpath '/' subname '.xml']);
+        ss.(subName).T0.MRI_sMRI = num2str(boganId);        
+        xmlpath=[copyDir subName '/xml_folder'];
+        struct2xml(ss,[xmlpath '/' subName '.xml']);
 
         % Copy the .nii.gz file and remove it from original location
-        system(['cp ' original_dir subjectID ' ' copy_dir subname '/Data/' num2str(bogan_id) '/' subjectID]);
+        system(['cp ' originalDir subjectID ' ' copyDir subName '/Data/' num2str(boganId) '/' subjectID]);
         % Zip the .nii file
-        %system(['gzip ' copy_dir subname '/Data/' num2str(bogan_id) '/' subname '.nii']);
-
+        %system(['gzip ' copyDir subName '/Data/' num2str(boganId) '/' subName '.nii']);
+    
     catch err
-        fprintf(fileID,'%s not copied\n',subjectID);
+        fprintf(FILE_ID,'%s not copied\n',subjectID);
         rethrow(err); 
-    end
-    bogan_id = bogan_id + 1;
+    end % end of try and catch
+    
+    boganId = boganId + 1;
     % Clear the structure for each image
     clear ind ss;
+end % end of files iteration loop
 
-end
-
-fclose(fileID);
+fclose(FILE_ID);
